f800306a6ce96da3b1fadcfaf0cb203a28918d49
Aidar042002 <71192036+Aidar042002@users.noreply.github.com>
2023-05-01 22:20:37 +0300

    add third version

--- app/reaction.php
+++ app/reaction.php
@@ -15,7 +15,7 @@
 
       <div class="buttons">
         <div class="setOfNumbers">
-
+          <!--1-->
           <button id="buttonReaction1" class="clickButton" type="button" name="button" onclick="clickButton(this)">Реакция на свет</button><br>
             <div id="reaction1" style="display:none;">
               <p>Оценка скорости реакции на свет</p>
@@ -28,23 +28,80 @@
               <div id="innerResultAttempt1"></div>
               <div id="innerResult1"></div> </div>
             </div>
-
+            <!--2-->
           <button id="buttonReaction2" class="clickButton" type="button" name="button" onclick="clickButton(this)">Реакция на звук</button><br>
             <div id="reaction2" style="display:none;">
               <p>Оценка реакции на звук</p>
-              <button id="startReaction2" type="button" name="button">Старт</button>
+              <button id="startReaction2" type="button" name="button" onclick="startReactionTwo()">Старт</button>
               <div>
                 Здесь работа со звуком
+                <audio id="audioOne">
+                  <source src="ul-trazvuk-pisk.mp3" type="audio/mp3">
+                </audio>
               </div>
-              <button id="clickButton2" type="button" name="button">Нажмите сюда</button><br>
+              <button id="clickButton2" type="button" name="button" onclick="clickButtonTwo()">Нажмите сюда</button><br>
               <button type="button" name="button" onclick="showAllButtons()">Назад к выбору теста</button>
+              <div id="testAttempt2"></div>
+              <div id="innerResultAttempt2"></div>
+              <div id="innerResult2"></div>
             </div>
+
+            <!--3-->
           <button id="buttonReaction3" class="clickButton" type="button" name="button" onclick="clickButton(this)">Реакция на цвета</button><br>
-
+            <div id="reaction3" style="display:none;">
+              <p>Оценка реакции на несколько цветов</p>
+              <button id="startReaction3" type="button" name="button" onclick="startReactionThree()">Старт</button><br>
+              <div id="blocks1" class="blocks">
+                <div id="block1" class="block"></div>
+                <button type="button" name="button" onclick='changeColor("block1")'>Red</button>
+              </div>
+              <div id="blocks2" class="blocks">
+                <div id="block2" class="block"></div>
+                <button type="button" name="button" onclick="changeColor('block2')">Green</button>
+              </div>
+              <div id="blocks3" class="blocks">
+                <div id="block3" class="block"></div>
+                <button type="button" name="button" onclick="changeColor('block3')">Blue</button>
+              </div>
+              <br><br><button type="button" name="button" onclick="showAllButtons()">Назад к выбору теста</button>
+              <div id="testAttempt3"></div>
+              <div id="innerResultAttempt3"></div>
+              <div id="innerResult3"></div>
+            </div>
+            <!--4-->
           <button id="buttonReaction4" class="clickButton" type="button" name="button" onclick="clickButton(this)">Реакция на звук при сложении</button><br>
-
+            <div id="reaction4" style="display:none;">
+              <p>Оценка реакции на сложение со звуком</p>
+              <button id="startReaction4" type="button" name="button" onclick="startReactionFour()">Старт</button>
+              <div>
+                <audio id="audioTwo">
+                  <source src="сложение.mp3" type="audio/mp3">
+                </audio>
+                <br><button id="d41" type="button" name="button" onclick="clickButtonFour('d41')">Четное</button>
+                <button id="d42" type="button" name="button" onclick="clickButtonFour('d42')">Нечетное</button>
+              </div>
+              <br><button type="button" name="button" onclick="showAllButtons()">Назад к выбору теста</button>
+              <div id="testAttempt4"></div>
+              <div id="innerResultAttempt4"></div>
+              <div id='result44'></div>
+              <div id="innerResult4"></div>
+            </div>
+          <!--5-->
           <button id="buttonReaction5" class="clickButton" type="button" name="button" onclick="clickButton(this)">Реакция на сложение</button><br>
-
+          <div id="reaction5" style="display:none;">
+            <p>Оценка реакции на сложение</p>
+            <button id="startReaction5" type="button" name="button" onclick="startReactionFive()">Старт</button>
+            <div>
+              <div id="addDigits"></div>
+              <br><button id="d51" type="button" name="button" onclick="clickButtonFive('d51')">Четное</button>
+              <button id="d52" type="button" name="button" onclick="clickButtonFive('d52')">Нечетное</button>
+            </div>
+            <br><button type="button" name="button" onclick="showAllButtons()">Назад к выбору теста</button>
+            <div id="testAttempt5"></div>
+            <div id="innerResultAttempt5"></div>
+            <div id='result55'></div>
+            <div id="innerResult5"></div>
+          </div>
         </div>
       </div>
 
--- app/scripts/reaction.js
+++ app/scripts/reaction.js
@@ -108,21 +108,436 @@ function clickButtonOne(){
 }
 
 // 1 stop
+
+//2
+var trueClickCount2=0;
+var falseClickCount2=0;
+var falseTest2=0;
+var trueTest2=0;
+var startTime2;
+var stopTime2;
+var time2;
+var times2=new Array(30);
+var testAllCount2=0;
+var arrCount2;
+var sum2=0;
+var average2=0;
+var testAllCount3=0;
+var falseTest3=0;
+var trueTest3=0;
 function buttonTwo(){
   document.getElementById('reaction2').style.display ='block';
 }
+var audio = document.getElementById('audioOne');
+var interval2;
+var status;
+function startReactionTwo(){
 
+  interval2=setInterval(function(){
+      setTimeout(function(){
+        audio.play();
+        startTime2=new Date().getTime();
+        status=1;
+      },0);
+
+  }, 2000);
+}
+
+function clickButtonTwo(){
+  if(testAllCount2<3){
+    document.getElementById('testAttempt2').innerHTML ='Идут тестовые попытки';
+    if(status!=1){
+      falseTest2++;
+      testAllCount2++;
+      document.getElementById('innerResultAttempt2').innerHTML ='Ложное нажатие';
+    } else{
+      trueTest2++;
+      testAllCount2++;
+      document.getElementById('innerResultAttempt2').innerHTML ='Нажатие зачтено';
+      audio.pause();
+      status=0;
+    }
+    document.getElementById('innerResult2').innerHTML ='Зачтено:'+trueTest2 + '.Ложные:'+falseTest2;
+    if((trueClickCount2 == 0)&&(testAllCount2 == 3)){
+      document.getElementById('testAttempt2').innerHTML ='Приготовьтесь к основным попыткам';
+      document.getElementById('innerResultAttempt2').innerHTML ='';
+      document.getElementById('innerResult2').innerHTML ='';
+    }
+  }
+  else if(testAllCount2>2){
+    document.getElementById('testAttempt2').innerHTML = 'Идут основные попытки.';
+    if(status!=1){
+      document.getElementById('innerResultAttempt2').innerHTML ='Ложное нажатие';
+      falseClickCount2++;
+    } else{
+      trueClickCount2++;
+      document.getElementById('innerResultAttempt2').innerHTML ='Нажатие зачтено';
+      audio.pause();
+      stopTime2=new Date().getTime();
+      time2=(stopTime2-startTime2)/1000;
+      arrCount2 = trueClickCount2;
+      times2[arrCount2 -1]=time2;
+      status=0;
+    }
+    document.getElementById('innerResult2').innerHTML = 'Засчитано попыток:'+trueClickCount2+', ложных:'+falseClickCount2+" time:"+time2;
+    if(trueClickCount2==30){
+      audio.pause();
+      clearInterval(interval2);
+      audio.pause();
+
+      for(let i=0; i<times2.length;i++){
+        sum2+=times2[i];
+      }
+      average2 = (sum2/times2.length);
+      document.getElementById('startReaction2').disabled = true;
+      document.getElementById('clickButton2').disabled = true;
+      document.getElementById('innerResult2').innerHTML = 'Результаты: засчитано попыток:'+trueClickCount2+', ложных:'+falseClickCount2+", среднее время:"+average2.toFixed(2);
+    }
+  }
+}
+//2 stop
+
+//3
+var colors=['red','green','blue'];
+var blocksId=['block1','block2','block3'];
+var interval3;
+var color;
+var id;
+var time3;
+var timeStart3;
+var timeStop3;
+var times3=new Array(3);
+var sum3=0;
+var average3=0;
+var arrCount3=0;
+var trueClickCount3=0;
+var falseClickCount3=0;
 function buttonThree(){
-
+  document.getElementById('reaction3').style.display = 'block';
+}
+function startReactionThree(){
+  interval3 =setInterval(function(){
+  color = colors[Math.floor(Math.random()*colors.length)];
+  id=blocksId[Math.floor(Math.random()*blocksId.length)];
+  document.getElementById(id).style.backgroundColor = color;
+  timeStart3 = new Date().getTime();
+}, 1000);
+}
+function changeColor(colorBlockId){
+  if(testAllCount3<3){
+    document.getElementById('testAttempt3').innerHTML = 'Идут тестовые попытки';
+    switch (colorBlockId) {
+      case 'block1':
+        if(document.getElementById('block1').style.backgroundColor == 'red'){
+          trueTest3++;
+          testAllCount3++;
+          timeStop3=new Date().getTime();
+          time3=(timeStop3-timeStart3)/1000;
+          document.getElementById('innerResultAttempt3').innerHTML ='Верно выбран красный цвет, время:'+time3;
+        } else {
+          document.getElementById('innerResultAttempt3').innerHTML ='Блок-1 цвет неверный';
+          falseTest3++;
+          testAllCount3++;
+        }
+        break;
+      case 'block2':
+        if(document.getElementById('block2').style.backgroundColor == 'green'){
+          trueTest3++;
+          testAllCount3++;
+          timeStop3=new Date().getTime();
+          time3=(timeStop3-timeStart3)/1000;
+          document.getElementById('innerResultAttempt3').innerHTML ='Верно выбран зеленый цвет, время:'+time3;
+          } else {
+            document.getElementById('innerResultAttempt3').innerHTML ='Блок-2 цвет неверный';
+            falseTest3++;
+            testAllCount3++;
+        }
+          break;
+      case 'block3':
+        if(document.getElementById('block3').style.backgroundColor == 'blue'){
+          trueTest3++;
+          testAllCount3++;
+          timeStop3=new Date().getTime();
+          time3=(timeStop3-timeStart3)/1000;
+          document.getElementById('innerResultAttempt3').innerHTML ='Верно выбран синий цвет, время:'+time3;
+        } else {
+          document.getElementById('innerResultAttempt3').innerHTML ='Блок-3 цвет неверный';
+          falseTest3++;
+          testAllCount3++;
+        }
+        break;
+      default:
+    }
+    document.getElementById('innerResult3').innerHTML ='Счетчик тестовых попыток:'+testAllCount3;
+    if((trueClickCount3 == 0)&&(testAllCount3 == 3)){
+      document.getElementById('testAttempt3').innerHTML ='Приготовьтесь к основным попыткам';
+      document.getElementById('innerResultAttempt3').innerHTML ='';
+      document.getElementById('innerResult3').innerHTML ='';
+    }
+  } else if(testAllCount3>2){
+    document.getElementById('testAttempt3').innerHTML = 'Идут основные попытки.';
+  switch (colorBlockId) {
+    case 'block1':
+      if(document.getElementById('block1').style.backgroundColor == 'red'){
+        trueClickCount3++;
+        timeStop3=new Date().getTime();
+        time3=(timeStop3-timeStart3)/1000;
+        arrCount3=trueClickCount3;
+        times3[arrCount3 -1]=time3;
+        document.getElementById('innerResultAttempt3').innerHTML ='Верно выбран красный цвет, время:'+time3;
+      } else {
+        document.getElementById('innerResultAttempt3').innerHTML ='Блок-1 цвет неверный';
+        falseClickCount3++;
+      }
+      break;
+    case 'block2':
+      if(document.getElementById('block2').style.backgroundColor == 'green'){
+        trueClickCount3++;
+        timeStop3=new Date().getTime();
+        time3=(timeStop3-timeStart3)/1000;
+        arrCount3=trueClickCount3;
+        times3[arrCount3 -1]=time3;
+        document.getElementById('innerResultAttempt3').innerHTML ='Верно выбран зеленый цвет, время:'+time3;
+        } else {
+          document.getElementById('innerResultAttempt3').innerHTML ='Блок-2 цвет неверный';
+          falseClickCount3++;
+      }
+        break;
+    case 'block3':
+      if(document.getElementById('block3').style.backgroundColor == 'blue'){
+        trueClickCount3++;
+        timeStop3=new Date().getTime();
+        time3=(timeStop3-timeStart3)/1000;
+        arrCount3=trueClickCount3;
+        times3[arrCount3 -1]=time3;
+        document.getElementById('innerResultAttempt3').innerHTML ='Верно выбран синий цвет, время:'+time3;
+      } else {
+        document.getElementById('innerResultAttempt3').innerHTML ='Блок-3 цвет неверный';
+        falseClickCount3++;
+      }
+      break;
+    default:
+  }
+  document.getElementById('innerResult3').innerHTML = 'Засчитано:'+trueClickCount3+', ложных:'+falseClickCount3+', time:'+time3;
+  if(trueClickCount3 == 3){
+    for(let i=0; i<times3.length;i++){
+      sum3+=times3[i];
+    }
+    average3 = (sum3/times3.length);
+    document.getElementById('innerResultAttempt3').innerHTML ='';
+    document.getElementById('innerResult3').innerHTML ='Итоговый результат- засчитано:'+trueClickCount3+" ,ложных:"+falseClickCount3+", среднее время:"+average3.toFixed(2);
+    document.getElementById('startReaction3').disabled = true;
+    clearInterval(interval3);
+    document.getElementById('testAttempt3').innerHTML = '';
+    document.getElementById('block1').style.backgroundColor = 'white';
+    document.getElementById('block2').style.backgroundColor = 'white';
+    document.getElementById('block3').style.backgroundColor = 'white';
+    }
+  }
 }
 
+//3 stop
+
+//4
+var trueClickCount4=0;
+var falseClickCount4=0;
+var falseTest4=0;
+var trueTest4=0;
+var startTime4;
+var stopTime4;
+var time4;
+var times4=new Array(3);
+var testAllCount4=0;
+var arrCount4;
+var sum4=0;
+var average4=0;
+var testAllCount4=0;
+var falseTest4=0;
+var trueTest4=0;
+var interval4;
+var audioTwo;
+var status4;
+var trueAnswer4=0;
+var falseAnswer4=0;
 function buttonFour() {
-
+  document.getElementById('reaction4').style.display = 'block';
 }
+function startReactionFour(){
+  interval4 = setInterval(function(){
+    audioTwo=document.getElementById('audioTwo');
+    setTimeout(function(){
+      audioTwo.play();
+      startTime4 = new Date();
+      status4=1;
+    },0);
+  },3000);
+}
+function clickButtonFour(digitId40){
+  var digitId4=digitId40;
+  if(testAllCount4<3){
+    document.getElementById('testAttempt4').innerHTML='Идут тестовые попытки';
+    if(status4 != 1){
+      document.getElementById('innerResultAttempt4').innerHTML='Ложное нажатие';
+      falseTest4++;
+      testAllCount4++;
+    } else{
+      trueTest4++;
+      testAllCount4++;
+      audioTwo.pause();
+      audioTwo.load();
+      status4=0;
+      document.getElementById('innerResultAttempt4').innerHTML='Нажатие зачтено';
+      stopTime4=new Date();
+      time4=(stopTime4-startTime4-1000)/1000;
+    }
+    document.getElementById('innerResult4').innerHTML='Зачтено-'+trueTest4+',ложных-'+falseTest4+'time:'+time4;
+    if((trueClickCount4==0)&&(testAllCount4==3)){
+      document.getElementById('testAttempt4').innerHTML='Приготовьтесь к основным попыткам';
+      document.getElementById('innerResultAttempt4').innerHTML='';
+      document.getElementById('innerResult4').innerHTML='';
+    }
+  } else if(testAllCount4>2){
+    document.getElementById('testAttempt4').innerHTML ='Идут основные попытки';
+    if(status4 != 1){
+      falseClickCount4++;
+      document.getElementById('innerResultAttempt4').innerHTML='Ложное нажатие';
+    }else {
+      trueClickCount4++;
+      audioTwo.pause();
+      audioTwo.load();
+      status4=0;
+      document.getElementById('innerResultAttempt4').innerHTML='Нажатие зачтено';
+      stopTime4=new Date();
+      time4=(stopTime4-startTime4-1000)/1000;
+      arrCount4=trueClickCount4;
+      times4[arrCount4 -1]=time4;
+      if(digitId4 == 'd42'){
+        trueAnswer4++;
+      } else if(digitId4 == 'd41'){
+        falseAnswer4++;
+      }
+    }
+    document.getElementById('innerResult4').innerHTML = 'Зачтено-'+trueClickCount4+',ложных-'+falseClickCount4+'time:'+time4;
+    if(trueClickCount4 == 3){
+      for(let i=0; i<times4.length;i++){
+        sum4+=times4[i];
+      }
+      average4 = (sum4/times4.length);
+      document.getElementById('innerResultAttempt4').innerHTML ='';
+      document.getElementById('innerResult4').innerHTML ='Итоговый результат- засчитано:'+trueClickCount4+" ,ложных:"+falseClickCount4+", среднее время:"+average4.toFixed(2);
+      document.getElementById('result44').innerHTML ='Верных ответов:'+trueAnswer4+', неверных:'+falseAnswer4;
+      document.getElementById('startReaction4').disabled = true;
+      audioTwo.pause();
+      clearInterval(interval4);
+      audioTwo.pause();
+      document.getElementById('testAttempt4').innerHTML = '';
+
+      }
+  }
+}
+//4 stop
+
+//5
+var trueClickCount5=0;
+var falseClickCount5=0;
+var falseTest5=0;
+var trueTest5=0;
+var startTime5;
+var stopTime5;
+var time5;
+var times5=new Array(3);
+var testAllCount5=0;
+var arrCount5;
+var sum5=0;
+var average5=0;
+var testAllCount5=0;
+var falseTest5=0;
+var trueTest5=0;
+var interval5;
+var trueAnswer5=0;
+var falseAnswer5=0;
+var digitSum=0;
+var allCount5=0;
 
 function buttonFive() {
-
+  document.getElementById('reaction5').style.display = 'block';
 }
+function startReactionFive(){
+  interval5 = setInterval(function(){
+    digit1=(Math.random()*50).toFixed(0);
+    digit2=(Math.random()*50).toFixed(0);
+    digitSum =digit1+digit2;
+    startTime5=new Date();
+    document.getElementById('addDigits').innerHTML = 'Сложите '+digit1+'+'+digit2;
+  },4000);
+}
+
+function clickButtonFive(digitId5){
+  if(testAllCount5<3){
+    document.getElementById('testAttempt5').innerHTML = 'Идут тестовые попытки';
+    if((digitSum/2 == 0)&&(digitId5 == 'd51')){
+      trueTest5++;
+      testAllCount5++;
+      document.getElementById('innerResultAttempt5').innerHTML = 'Правильно';
+    } else if((digitSum/2 != 0)&&(digitId5 == 'd52')){
+      trueTest5++;
+      testAllCount5++;
+      document.getElementById('innerResultAttempt5').innerHTML = 'Правильно';
+    } else {
+      falseTest5++;
+      testAllCount5++;
+      document.getElementById('innerResultAttempt5').innerHTML = 'Неправильно';
+    }
+    document.getElementById('innerResult5').innerHTML ='Результаты: правильно'+trueTest5+', неправильно:'+falseTest5;
+    if((testAllCount5==3)&&(trueClickCount5 ==0)){
+      document.getElementById('testAttempt5').innerHTML = 'Приготовьтесь к основным попыткам';
+      document.getElementById('innerResult5').innerHTML = '';
+      document.getElementById('innerResultAttempt5').innerHTML = '';
+    }
+  } else if(testAllCount5>2){
+    document.getElementById('testAttempt5').innerHTML = 'Идут основные попытки';
+    if((digitSum/2 == 0)&&(digitId5 == 'd51')){
+      trueClickCount5++;
+      allCount5++;
+      stopTime5=new Date();
+      time5=(stopTime5-startTime5)/1000;
+      arrCount5=allCount5;
+      times5[arrCount5 -1]=time5;
+      document.getElementById('innerResultAttempt5').innerHTML = 'Правильно';
+    } else if((digitSum/2 != 0)&&(digitId5 == 'd52')){
+      trueClickCount5++;
+      allCount5++;
+      stopTime5=new Date();
+      time5=(stopTime5-startTime5)/1000;
+      arrCount5=allCount5;
+      times5[arrCount5 -1]=time5;
+      document.getElementById('innerResultAttempt5').innerHTML = 'Правильно';
+    } else {
+      falseClickCount5++;
+      allCount5++;
+      stopTime5=new Date();
+      time5=(stopTime5-startTime5)/1000;
+      arrCount5=allCount5;
+      times5[arrCount5 -1]=time5;
+      document.getElementById('innerResultAttempt5').innerHTML = 'Неправильно';
+    }
+    document.getElementById('innerResult5').innerHTML ='Результаты: правильно'+trueClickCount5+', неправильно:'+falseClickCount5+', time:'+time5;
+    if(allCount5 == 3){
+      for(let i=0; i<times5.length;i++){
+        sum5+=times5[i];
+      }
+      average5 = (sum5/times5.length);
+      document.getElementById('innerResultAttempt5').innerHTML ='';
+      document.getElementById('innerResult5').innerHTML ='Итоговый результат- засчитано:'+trueClickCount5+" ,ложных:"+falseClickCount5+", среднее время:"+average5.toFixed(2);
+      document.getElementById('startReaction5').disabled = true;
+      clearInterval(interval5);
+      document.getElementById('addDigits').style.display ='none';
+      document.getElementById('testAttempt4').innerHTML = '';
+    }
+  }
+}
+
+//5 stop
 
 function hideAllButtons(){
   document.getElementById('buttonReaction1').style.display = 'none';
@@ -135,6 +550,9 @@ function hideAllButtons(){
 function showAllButtons(){
   document.getElementById('reaction1').style.display = 'none';
   document.getElementById('reaction2').style.display = 'none';
+  document.getElementById('reaction3').style.display = 'none';
+  document.getElementById('reaction4').style.display = 'none';
+  document.getElementById('reaction5').style.display = 'none';
 
   document.getElementById('buttonReaction1').style.display = 'block';
   document.getElementById('buttonReaction2').style.display = 'block';
@@ -142,25 +560,3 @@ function showAllButtons(){
   document.getElementById('buttonReaction4').style.display = 'block';
   document.getElementById('buttonReaction5').style.display = 'block';
 }
-
-// if(color== 'white'){
-//   document.getElementById('innerResultAttempt1').innerHTML = 'Ложное нажатие';
-//   falseClickCount1++;
-// } else{
-//   trueClickCount1++;
-//   if(trueClickCount1>0){
-//   document.getElementById('innerResultAttempt1').innerHTML = 'Попытка засчитана';
-//   stopTime1 = new Date().getTime();
-//   time1 = (stopTime1-startTime1)/1000;
-//   } else{
-//
-//     }
-//
-// }
-// if(trueClickCount1>0){
-// document.getElementById('innerResult1').innerHTML = 'Результаты: засчитано нажатий:'+trueClickCount1 +'. Ложных нажатий:'+falseClickCount1+".Время: "+ time1;
-// }
-// if(trueClickCount1 == 3) {
-//   clearInterval(interval1);
-//
-// }
